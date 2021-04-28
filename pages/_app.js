eb0695b5e003d02b31896da00c10c5227c6c9639
demianma <tobata@gmail.com>
2021-04-28 11:42:44 -0300

    teste

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+// add bootstrap css 
+import 'bootstrap/dist/css/bootstrap.css'
+
+// own css files here
+import "../css/customcss.css";
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
